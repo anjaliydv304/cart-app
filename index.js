98cf7590b2dcd6c96516d1276f27ff928ecce2a3
anjaliydv304 <anjali.ydv304@gmail.com>
2024-04-30 01:32:00 +0530

    firebase db

---
+++ index.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase,ref,push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js" 
+const appSettings={
+    databaseURL:"https://cart-app-beabc-default-rtdb.asia-southeast1.firebasedatabase.app"    
+}
+
+const app = initializeApp(appSettings)
+const database= getDatabase(app)
+const brandsinDB = ref(database,"luxury")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(brandsinDB, inputValue)
+    
+    console.log(`${inputValue} added to database`)
+})
